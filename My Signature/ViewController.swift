50153a08c09f237db6c609dd825f8bc2d7066fde
omrobbie <putu.roby@yahoo.com>
2020-02-24 14:07:45 +0800

    setup layout

--- My Signature/ViewController.swift
+++ My Signature/ViewController.swift
@@ -7,10 +7,20 @@
 //
 
 import UIKit
+import SignaturePad
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signaturePad: SignaturePad!
+    @IBOutlet weak var imgSignature: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func btnSimpanTapped(_ sender: Any) {
+    }
+
+    @IBAction func btnUlangiTapped(_ sender: Any) {
     }
 }
